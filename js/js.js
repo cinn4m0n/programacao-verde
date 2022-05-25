ffdb2fab272742b1023de98fd4d91f3043bc04b4
cinn4m0n <105285257+cinn4m0n@users.noreply.github.com>
2022-05-25 08:43:07 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,55 @@
+// Pego os Elementos do Li
+var btn = document.querySelectorAll(".key li"),
+    input = document.querySelector(".input-valor"),
+    operador = document.querySelectorAll(".operador");
+
+//Percorro a lista para ver e comparar 
+//se as teclas foram clicadas.
+for(var i = 0;  i < btn.length; i++){
+    document.onkeypress = function(event){
+        for(var e = 0; e <= 10; e++){
+            if(key === (48+e)){
+                input.innerHTML += e;
+            }
+        }
+        switch(key){
+            case 42:
+                input.innerHTML += "*";
+                break;
+            case 43:
+                input.innerHTML += "+";
+                break;
+            case 45:
+                input.innerHTML += "-";
+                break;
+            case 46:
+                input.innerHTML += ".";
+                break;
+            case 47:
+                input.innerHTML += "/";
+                break;
+            case 13:
+            case 61:
+                var equacao = input.innerHTML;
+                if(equacao){
+                    try{
+                        input.innerHTML = eval (equacao);
+                    } catch (e){
+                        alert('Erro na Expressão');
+                    }
+                }
+                break;
+                case 67:
+                case 99:
+                    input.innerHTML = "";
+                    break;
+                    default:
+                    break;
+        }
+    }
+
+
+};
+  
+
+
